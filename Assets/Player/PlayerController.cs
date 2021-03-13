27c80102814d392947efd89ba544ebd2857c2e3a
Hiroto-Terai <hiro08051610@gmail.com>
2021-03-14 07:02:08 +0900

    update: プレイヤーの横移動処理を実装した

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  Rigidbody2D rbody;
+  float axisH = 0.0f;
+  public float speed = 3.0f;
 
-    // Update is called once per frame
-    void Update()
+  // Start is called before the first frame update
+  void Start()
+  {
+    rbody = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    axisH = Input.GetAxisRaw("Horizontal");
+    if (axisH > 0.0f)
     {
-        
+      Debug.Log("右移動");
+      transform.localScale = new Vector2(1, 1);
     }
+    else if (axisH < 0.0f)
+    {
+      Debug.Log("左移動");
+      transform.localScale = new Vector2(-1, 1); // 左右反転
+    }
+  }
+
+  void FixedUpdate()
+  {
+    // 速度更新
+    rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
+  }
 }
