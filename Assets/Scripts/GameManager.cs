3e681673fac0664a43c575a69e93fc4c49132d1b
Hiroto-Terai <hiro08051610@gmail.com>
2021-03-20 15:16:00 +0900

    update: カウントダウンを画面上に表示

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,11 @@ public class GameManager : MonoBehaviour
 
   Image titleImage;
 
+  // +++ 時間制限追加 +++ 
+  public GameObject timeBar;  // 時間表示イメージ
+  public GameObject timeText;  // 時間テキスト
+  TimeController timeCnt;  // TimeController
+
   // Start is called before the first frame update
   void Start()
   {
@@ -21,6 +26,17 @@ public class GameManager : MonoBehaviour
     Invoke("InactiveImage", 1.0f);
     // ボタン（パネル）を非表示にする
     panel.SetActive(false);
+
+    // +++ 時間制限追加 +++ 
+    // TimeControllerを取得
+    timeCnt = GetComponent<TimeController>();
+    if (timeCnt != null)
+    {
+      if (timeCnt.gameTime == 0.0f)
+      {
+        timeBar.SetActive(false);  // 制限時間なしなら非表示
+      }
+    }
   }
 
   // Update is called once per frame
@@ -36,6 +52,12 @@ public class GameManager : MonoBehaviour
       bt.interactable = false;
       mainImage.GetComponent<Image>().sprite = gameClearSpr;  // 画像設定
       PlayerController.gameState = "gameend";
+
+      // +++ 時間制限追加 +++
+      if (timeCnt != null)
+      {
+        timeCnt.isTimeOver = true;  // 時間カウント停止
+      }
     }
     else if (PlayerController.gameState == "gameover")
     {
@@ -47,10 +69,36 @@ public class GameManager : MonoBehaviour
       bt.interactable = false;
       mainImage.GetComponent<Image>().sprite = gameOverSpr;  // 画像設定
       PlayerController.gameState = "gameend";
+
+      // +++ 時間制限追加 +++
+      if (timeCnt != null)
+      {
+        timeCnt.isTimeOver = true;  // 時間カウント停止
+      }
     }
     else if (PlayerController.gameState == "playing")
     {
       // ゲーム中
+      GameObject player = GameObject.FindGameObjectWithTag("Player");
+      // PlayerControllerを取得
+      PlayerController playerCnt = player.GetComponent<PlayerController>();
+      // +++ 時間制限追加 +++
+      // タイムを更新する
+      if (timeCnt != null)
+      {
+        if (timeCnt.gameTime > 0.0f)
+        {
+          // 整数に代入することで少数を切り捨て
+          int time = (int)timeCnt.displayTime;
+          // タイム更新
+          timeText.GetComponent<Text>().text = time.ToString();
+          // タイムオーバー
+          if (time == 0)
+          {
+            playerCnt.GameOver();  // ゲームオーバーにする
+          }
+        }
+      }
     }
   }
   void InactiveImage()
